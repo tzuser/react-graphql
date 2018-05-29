cc9efc635527aac2e7ea2cc1e2b94b87400f4490
tzuser <tzuser@yeah.net>
2018-05-29 15:17:12 +0800

    修改请求地址

--- src/public.js
+++ src/public.js
@@ -1,5 +1,5 @@
 //192.168.1.105 otaku.tangzuo.cc
-export const HOST='http://192.168.253.1:8181';
+export const HOST='http://otaku.tangzuo.cc';
 export const IMG_HOST=HOST;
 export const DB_URL=`${HOST}/graphql`;
 export const imageUrl=(url)=>{
