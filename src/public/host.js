863b4310e00c4627637ac031f5e0b409baa4ed4e
tzuser <tzuser@yeah.net>
2018-07-07 18:18:40 +0800

    修改请求地址

--- src/public/host.js
+++ src/public/host.js
@@ -1,4 +1,4 @@
 //http://otaku.tangzuo.cc
 export const HOST='otaku.tangzuo.cc';
-export const IMG_URL='http://otaku.tangzuo.cc';
-export const DB_URL=`http://otaku.tangzuo.cc/graphql`;
+export const IMG_URL='http://otaku.tangzuo.cc:8184';
+export const DB_URL=`http://otaku.tangzuo.cc:8184/graphql`;
