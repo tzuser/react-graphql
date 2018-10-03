04b36081d6bddd171caffb968b71f8ecb4f7ec49
tzuser <tzuser@yeah.net>
2018-10-03 10:16:49 +0800

    修改

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,10 @@
-import React,{Component} from 'react';
-import {Box,Icon,SearchField,IconButton,Button,Text} from 'gestalt';
+import React, { Component } from 'react';
+import { Box, Icon, SearchField, IconButton, Button, Text } from 'gestalt';
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import HeaderContainer from 'com_/HeaderContainer';
 
-const SearchButton=styled(Link)`
+const SearchButton = styled(Link)`
   transition: transform 0.2s ease-out;
   background-color: #efefef;
   border-radius: 8px;
@@ -17,34 +17,34 @@ const SearchButton=styled(Link)`
   text-decoration: none;
   flex: 1 1 auto;
   align-items: center;
-  &:active{
-    border:1px solid #bbb;
+  &:active {
+    border: 1px solid #bbb;
     padding-left: 11px;
     padding-right: 11px;
   }
-`
+`;
 
-class Header extends Component{
-  state={value:''}
-  render(){
+class Header extends Component {
+  state = { value: '' };
+  componentDidMount() {
+    console.log('Did Mount');
+  }
+  render() {
     return (
       <HeaderContainer>
-            <SearchButton to="/search_keyword/">
-              <Box padding={1} >
-                <Icon 
-                icon="search"
-                size={16}
-                accessibilityLabel="Pinterest"
-                />
-              </Box>
-              <Box flex="grow" padding={3} >
-                <Text bold color="gray">搜索</Text>
-              </Box>
-            </SearchButton>
+        <SearchButton to="/search_keyword/">
+          <Box padding={1}>
+            <Icon icon="search" size={16} accessibilityLabel="Pinterest" />
+          </Box>
+          <Box flex="grow" padding={3}>
+            <Text bold color="gray">
+              搜索
+            </Text>
+          </Box>
+        </SearchButton>
       </HeaderContainer>
     );
   }
 }
-
 
 export default Header;
--- src/Containers/Home.jsx
+++ src/Containers/Home.jsx
@@ -1,42 +1,47 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
-import { Box,Spinner } from 'gestalt';
+import { Box, Spinner } from 'gestalt';
 import Header from 'com_/Header';
 import PostList from 'com_/PostList';
-import {withRouter} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 //import RestoredScroll from 'com_/RestoredScroll';
-import homePostsQuery from 'gql_/homePosts.gql'
-import {loadItems} from '_public';
+import homePostsQuery from 'gql_/homePosts.gql';
+import { loadItems } from '_public';
 import InTheEnd from 'com_/InTheEnd';
 @withRouter
-@graphql(homePostsQuery,{
-  options:(props)=>{
-      return {
-      variables:{
-        first:20
-      }
-  }},
+@graphql(homePostsQuery, {
+  options: props => {
+    return {
+      variables: {
+        first: 20,
+      },
+    };
+  },
 })
-class Home extends React.Component{
-  constructor(props){
+class Home extends React.Component {
+  constructor(props) {
     super(props);
   }
-  render(){
-    let { data: { posts, refetch ,fetchMore,loading},history:{push} }=this.props;
+  render() {
+    let {
+      data: { posts, refetch, fetchMore, loading },
+      history: { push },
+    } = this.props;
     return (
       <div>
-        <Header/>
-          <PostList 
-          list={posts?posts.list:[]}
-          loadItems={(data)=>loadItems({props:this.props,queryName:'posts'})}
-          />
+        <Header />
+        <PostList
+          list={posts ? posts.list : []}
+          loadItems={data =>
+            loadItems({ props: this.props, queryName: 'posts' })
+          }
+        />
         <Spinner show={loading} accessibilityLabel="Example spinner" />
-        {posts && posts.isEnd && <InTheEnd/>}
-     </div>
+        {posts && posts.isEnd && <InTheEnd />}
+      </div>
     );
   }
 }
-
 
 export default Home;
